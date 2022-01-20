39b7f3c873f009710b69a5ec7486bcab6685d989
Lílian <lily.mosquitoes@gmail.com>
2022-01-21 01:09:12 +0200

    basic setup

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use rocket_sync_db_pools::{diesel, database};
+
+pub mod routes;
+
+#[database("db")]
+struct DbConn(diesel::PgConnection);
+
+#[launch]
+fn rocket() -> _ {
+    dotenv().ok();
+    rocket::build().attach(DbConn::fairing()).mount("/", routes![routes::index])
+}
