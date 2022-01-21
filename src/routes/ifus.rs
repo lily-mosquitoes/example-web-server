a2b26c63d64fafa64019d731c56d13a6a4ef040b
Lílian <lily.mosquitoes@gmail.com>
2022-01-21 23:06:55 +0200

    organized routes::ifus

--- src/routes/ifus.rs
+++ src/routes/ifus.rs
@@ -2,37 +2,37 @@ use rocket::http::Status;
 use rocket::response::status;
 use rocket::serde::json::Json;
 use crate::connection::DbConn;
-use crate::models;
-use crate::repository;
+use crate::models::Ifu;
+use crate::repository::ifus;
 use crate::routes::utils::{ error_status, record_created };
 
 #[get("/ifus")]
-pub async fn all(connection: DbConn) -> Result<Json<Vec<models::Ifu>>, Status> {
-    connection.run( |c| repository::ifus::all(c)
+pub async fn all(connection: DbConn) -> Result<Json<Vec<Ifu>>, Status> {
+    connection.run( |c| ifus::all(c)
         .map(|ifus| Json(ifus))
         .map_err(|error| error_status(error))
     ).await
 }
 
 #[get("/ifu/<id>")]
-pub async fn get(id: i32, connection: DbConn) -> Result<Json<models::Ifu>, Status> {
-    connection.run( move |c| repository::ifus::get(id, c)
+pub async fn get(id: i32, connection: DbConn) -> Result<Json<Ifu>, Status> {
+    connection.run( move |c| ifus::get(id, c)
         .map(|ifu| Json(ifu))
         .map_err(|error| error_status(error))
     ).await
 }
 
 #[post("/ifu", format="application/json", data="<ifu>")]
-pub async fn post(ifu: Json<models::Ifu>, connection: DbConn) -> Result<status::Created<Json<models::Ifu>>, Status> {
-    connection.run( |c| repository::ifus::insert(ifu.into_inner(), c)
+pub async fn post(ifu: Json<Ifu>, connection: DbConn) -> Result<status::Created<Json<Ifu>>, Status> {
+    connection.run( |c| ifus::insert(ifu.into_inner(), c)
         .map(|ifu| record_created(ifu))
         .map_err(|error| error_status(error))
     ).await
 }
 
 #[put("/ifu/<id>", format="application/json", data="<ifu>")]
-pub async fn update(id: i32, ifu: Json<models::Ifu>, connection: DbConn) -> Result<Json<models::Ifu>, Status> {
-    connection.run( move |c| repository::ifus::update(id, ifu.into_inner(), c)
+pub async fn update(id: i32, ifu: Json<Ifu>, connection: DbConn) -> Result<Json<Ifu>, Status> {
+    connection.run( move |c| ifus::update(id, ifu.into_inner(), c)
         .map(|ifu| Json(ifu))
         .map_err(|error| error_status(error))
     ).await
@@ -40,8 +40,8 @@ pub async fn update(id: i32, ifu: Json<models::Ifu>, connection: DbConn) -> Resu
 
 #[delete("/ifu/<id>")]
 pub async fn delete(id: i32, connection: DbConn) -> Result<status::NoContent, Status> {
-    connection.run( move |c| match repository::ifus::get(id, c) {
-        Ok(_) => repository::ifus::delete(id, c)
+    connection.run( move |c| match ifus::get(id, c) {
+        Ok(_) => ifus::delete(id, c)
             .map(|_| status::NoContent)
             .map_err(|error| error_status(error)),
         Err(error) => Err(error_status(error))
