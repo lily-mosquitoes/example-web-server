4de29be760eca63d289e31978b258464ae9267da
Lílian <lily.mosquitoes@gmail.com>
2022-01-22 05:57:24 +0200

    implemented user model

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 pub mod ifus;
 pub mod products;
+pub mod users;
 
 pub trait Record {
     fn id(&self) -> i32;
---
+++ src/models/users.rs
@@ -0,0 +1,33 @@
+use rocket::serde::{ Serialize, Deserialize };
+use diesel::{ Queryable, Insertable };
+use chrono::{Local, DateTime, TimeZone};
+use crate::schema::users;
+use crate::models::Record;
+
+#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password_hash: String,
+    pub last_login: DateTime<Local>,
+}
+
+impl Record for User {
+    fn id(&self) -> i32 { self.id }
+}
+
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct InsertableUser {
+    pub username: String,
+    pub password_hash: String,
+}
+
+impl InsertableUser {
+    pub fn from_user(user: User) -> InsertableUser {
+        InsertableUser {
+            username: user.username,
+            password_hash: user.password_hash,
+        }
+    }
+}
