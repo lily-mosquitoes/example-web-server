03b7455d33d4b4e7a7251838f6239f0e5ce6de07
Lílian <lily.mosquitoes@gmail.com>
2022-01-26 16:24:49 +0200

    added favicon

--- src/routes/website.rs
+++ src/routes/website.rs
@@ -1,6 +1,7 @@
 use rocket_dyn_templates::Template;
 use std::collections::HashMap;
 use rocket::form::Form;
+use rocket::http::Status;
 use crate::connection::DbConn;
 use crate::models::ifus::Ifu;
 use crate::repository::ifus;
@@ -18,16 +19,22 @@ pub fn index() -> Template {
 }
 
 #[post("/", format="multipart/form-data", data="<search>")]
-pub async fn search(search: Form<Search>, connection: DbConn) -> Template {
+pub async fn search(search: Form<Search>, connection: DbConn) -> (Status, Template) {
     let mut context = HashMap::<String, i32>::new();
     let code = search.into_inner().code;
 
     let result = connection.run( move |c| ifus::search(code, c)).await;
 
     match result {
-        Ok(id) => context.insert("ifu_id".to_string(), id),
-        Err(error) => context.insert("not_found".to_string(), 404),
-    };
+        Ok(id) => {
+            context.insert("ifu_id".to_string(), id);
+            (Status::Ok, Template::render("index", &context))
+        },
+        Err(error) => {
+            context.insert("not_found".to_string(), 404);
+            (Status::NotFound, Template::render("index", &context))
+        },
+    }
 
-    Template::render("index", &context)
+    // Template::render("index", &context)
 }
